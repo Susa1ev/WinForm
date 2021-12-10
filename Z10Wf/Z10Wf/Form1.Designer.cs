3bc168e92ee377ddc96b21dedab481bf7484e01b
Susa1ev <susalytt@gmail.com>
2021-12-10 23:29:02 +0300

    AddZ10WF

---
+++ Z10Wf/Z10Wf/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Z10Wf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(472, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 55);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(421, 320);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.Location = new System.Drawing.Point(472, 129);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(619, 198);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "По нажатию кнопки будут выполнены следующие действия, выводы будут находиться зде" +
+    "сь\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1168, 450);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ Z10Wf/Z10Wf/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Z10Wf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Main()
+        {
+            try
+            {
+
+                string strTxt1 = "Иванов Иван Иванович, 1965 года рождения, место жительства г. Саратов", strTxt2 = "Петров Сергей Федорович, 1966 года рождения, место жительства г.Энгельс";
+                StringBuilder strTxt3 = new StringBuilder();
+                string path1 = @"C:\temp\K1\";
+                string path2 = @"C:\temp\K2\";
+                DirectoryInfo dirInfo = new DirectoryInfo(@"C:\temp");
+                richTextBox2.Text = string.Empty;
+                strTxt3.Append(strTxt1);
+                strTxt3.Append("\n");
+                strTxt3.Append(strTxt2);
+
+                if (!dirInfo.Exists)
+                {
+                    dirInfo.Create();
+                }
+                else
+                {
+                    dirInfo.Delete(true);
+                    dirInfo.Create();
+                }                
+                dirInfo.CreateSubdirectory("K1");
+                FileStream txt1 = File.Create(path1 + @"\t1.txt");
+                txt1.Close();
+                StreamWriter sw1 = new StreamWriter(path1 + @"\t1.txt");
+                sw1.WriteLine(strTxt1);
+                dirInfo.CreateSubdirectory("K2");
+                FileStream txt2 = File.Create(path1 + @"\t2.txt");
+                txt2.Close();
+                FileStream txt3 = File.Create(path2 + @"\t3.txt");
+                txt3.Close();
+                StreamWriter sw2 = new StreamWriter(path1 + @"\t2.txt", false);
+                StreamWriter sw3 = new StreamWriter(path2 + @"\t3.txt", false);
+                sw2.WriteLine(strTxt2);
+                sw1.Close();
+                sw2.Close();
+                sw3.Write(strTxt3.ToString());
+                sw3.Close();
+                StreamReader sr1 = new StreamReader(path1 + @"\t1.txt");
+                richTextBox2.AppendText(@"Первый файл с распололжением C:\temp\K1\t1.txt"); richTextBox2.AppendText("\n");                
+                richTextBox2.AppendText(sr1.ReadToEnd().ToString()); richTextBox2.AppendText("\n");
+                richTextBox2.AppendText(@"Второй файл с распололжением C:\temp\K1\t2.txt"); richTextBox2.AppendText("\n");
+                StreamReader sr2 = new StreamReader(path1 + @"\t2.txt");
+                richTextBox2.AppendText(sr2.ReadToEnd().ToString()); richTextBox2.AppendText("\n");
+                richTextBox2.AppendText(@"Третий файл с распололжением C:\temp\K2\t3.txt"); richTextBox2.AppendText("\n");
+                StreamReader sr3 = new StreamReader(path2 + @"\t3.txt");
+                richTextBox2.AppendText(sr3.ReadToEnd().ToString()); richTextBox2.AppendText("\n");
+                sr1.Close();
+                sr2.Close();
+                sr3.Close();
+                File.Move(path1 + @"\t1.txt", path2 + @"\t1.txt");
+                File.Move(path1 + @"\t2.txt", path2 + @"\t2.txt");
+                Directory.Move(path2, @"C:\temp\ALL");
+                Directory.Delete(@"C:\temp\K1", true);
+                string[] files = Directory.GetFiles(@"C:\temp\ALL");
+                foreach (string File in files)
+                {
+                    richTextBox2.AppendText(File); richTextBox2.AppendText("\n");
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Какая-то ошибка");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Main();
+        }
+    }
+}
