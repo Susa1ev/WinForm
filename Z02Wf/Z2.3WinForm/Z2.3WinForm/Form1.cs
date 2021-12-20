79eb8fcaff14cdd936ce6d9ce1f9031a3341eae9
Андрей <95218345+Susa1ev@users.noreply.github.com>
2021-12-20 21:36:59 +0300

    Add files via upload

---
+++ Z02Wf/Z2.3WinForm/Z2.3WinForm/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Z2._3WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int a, b;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Main();
+        }
+
+        void Main()
+        {
+            if (Int32.TryParse(textBox1.Text, out a) && Int32.TryParse(textBox2.Text, out b) && a < b)
+            {
+                label3.Text = string.Empty;
+                label3.Text += "Через For:\t\t";
+                for (int i = a; i < b; i++)
+                {
+                    if (i % 3 == 0)
+                    {
+                        label3.Text += i + " ";
+                    }
+                }
+                label3.Text += "\nЧерез While:\t\t";
+                int wi = a;
+                while (wi < b)
+                {
+                    if (wi % 3 == 0)
+                    {
+                        label3.Text += wi + " ";
+                    }
+                    wi++;
+                }
+                label3.Text += "\nЧерез do...While:\t";
+                int dwi = a;
+                do
+                {
+                    if (dwi % 3 == 0)
+                    {
+                        label3.Text += dwi + " ";
+                    }
+                    dwi++;
+                } while (dwi < b);
+            }
+            else
+            {
+                MessageBox.Show("Вы ввели что-то не то");
+            }
+
+        }
+    }
+}
---
+++ Z02Wf/Z2.4WinForm/Z2.4WinForm/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Z2.4WinForm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Z2.4WinForm")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f7dc6d43-6bf3-444b-ae3c-0b179fedf72c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
